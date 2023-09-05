3627f64115d85411de802ac233e169e5ade807f5
Dheeraj P <dheerajp@Dheerajs-MacBook-Pro.local>
2023-09-06 04:28:53 +0530

    first commit

---
+++ src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from '../styling/landing.module.css'
+
+function Landing() {
+  return (
+    <div className={style.container}>
+    
+    </div>
+  )
+}
+
+export default Landing
